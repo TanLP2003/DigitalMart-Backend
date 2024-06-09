ab8f772bea367478551bd2c7ffc84723a688d283
TanLP2003 <phutanle372@gmail.com>
2024-06-09 23:04:23 +0700

    Updated

--- src/features/user/user.route.js
+++ src/features/user/user.route.js
@@ -11,7 +11,7 @@ userRouter.post('/login', Validation(loginRequestValidate), UserController.login
 userRouter.post('/logout', Authentication(CONSTANT.ROLE.ALL), UserController.logout);
 userRouter.post('/refreshToken', Authentication(CONSTANT.ROLE.ALL), Validation(refreshTokenReqValidate), UserController.refreshToken);
 userRouter.put('/changeAvatar', Authentication(CONSTANT.ROLE.ALL), uploadMulter.single('avatar'), UserController.changeAvatar);
-userRouter.put('/updateInfo', Authentication(CONSTANT.ROLE.CUSTOMER), Validation(updateUserInfoReqValidate), UserController.updateUserInfo);
+userRouter.put('/updateInfo', Authentication(CONSTANT.ROLE.ALL), Validation(updateUserInfoReqValidate), UserController.updateUserInfo);
 userRouter.put('/changePassword', Authentication(CONSTANT.ROLE.ALL), Validation(changePasswordReqValidate), UserController.changePassword);
 userRouter.get('/verify/:token', UserController.verify)
 userRouter.put('/forgotPassword', Validation(forgotPasswordReqValidate), UserController.forgotPassword);
